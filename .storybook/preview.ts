82dd5e91e34310a22d37c5e2187bc99003822051
ZanziPanziBY <zanzipanziby@gmail.com>
2024-04-21 22:16:59 +0300

    feat: import global styles and fonts in project and storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,23 @@
-import type { Preview } from "@storybook/react";
+import type {Preview} from "@storybook/react";
+import '@fontsource-variable/lora';
+import '@fontsource-variable/lora/wght-italic.css';
+import '@fontsource/roboto/100.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+import '../src/styles/globals.scss';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/900.css';
-import "./globals.css";
+import "../styles/globals.scss";
 import {ReactNode} from "react";
 
 
